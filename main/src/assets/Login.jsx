9005b057846f542882d4a387fca847c6ec22f37e
Abhinav352 <abhinav0298@gmail.com>
2024-02-21 21:15:14 +0530

    Login.css changes

--- main/src/assets/Login.jsx
+++ main/src/assets/Login.jsx
@@ -1,85 +1,81 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { Map, Marker } from "pigeon-maps"
 import './Login.css';
-
 
 const Login = () => {
   const [userEmail, setEmail] = useState('');
   const [userPassword, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
-  const navigate = useNavigate(); // Replace useHistory with useNavigate
-  const [authenticated, setauthenticated] = useState(null)
-  
+  const navigate = useNavigate();
+  const [authenticated, setauthenticated] = useState(null);
 
-
-  useEffect(()=>{
+  useEffect(() => {
     const loggedInUser = localStorage.getItem("authenticated");
-    setauthenticated(loggedInUser)
-},[])
+    setauthenticated(loggedInUser);
+  }, []);
 
-const handleSignIn = async () => {
-  try {
-    setLoading(true);
+  const handleSignIn = async () => {
+    try {
+      setLoading(true);
 
-    const response = await axios.post('http://localhost:5000/login', {
-      userEmail,
-      userPassword,
-    });
+      const response = await axios.post('http://localhost:5000/login', {
+        userEmail,
+        userPassword,
+      });
 
-    const data = response.data;
-    localStorage.setItem('authenticated', true);
-    localStorage.setItem('user',JSON.stringify(data.user));
-    localStorage.setItem('token',JSON.stringify(data.token));
-    localStorage.setItem('users',JSON.stringify(data.user._id));
-    localStorage.setItem('userType',JSON.stringify(data.user.userType));
-    localStorage.setItem('userEmail',JSON.stringify(data.user.userEmail));
-    localStorage.setItem('userName',JSON.stringify(data.user.firstName));
-    console.log(data);
-    setEmail(data.user.userEmail);
+      const data = response.data;
+      localStorage.setItem('authenticated', true);
+      localStorage.setItem('user', JSON.stringify(data.user));
+      localStorage.setItem('token', JSON.stringify(data.token));
+      localStorage.setItem('users', JSON.stringify(data.user._id));
+      localStorage.setItem('userType', JSON.stringify(data.user.userType));
+      localStorage.setItem('userEmail', JSON.stringify(data.user.userEmail));
+      localStorage.setItem('userName', JSON.stringify(data.user.firstName));
+      setEmail(data.user.userEmail);
 
-    if (response.status === 200) {
-      
-      // Redirect to the home page upon successful login
-      navigate('/');
-    } else {
-      // Handle unsuccessful login (you might want to provide more specific error messages)
-      setError('Invalid username or password');
+      if (response.status === 200) {
+        navigate('/');
+      } else {
+        setError('Invalid username or password');
+      }
+    } catch (error) {
+      setError('Error during login. Please try again.');
+      console.error('Error during login:', error);
+    } finally {
+      setLoading(false);
     }
-  } catch (error) {
-    setError('Error during login. Please try again.');
-    console.error('Error during login:', error);
-  } finally {
-    setLoading(false);
-  }
-};
+  };
+
+  const handleSignUp = () => {
+    navigate('/Sign');
+  };
 
   return (
-    <div>
-    
-      <h2 className='head'>Login</h2>
-      <div className='dk'><label className='jl'>Email : </label>
-        <input type="email"  value={userEmail} onChange={(e) => setEmail(e.target.value)}  />
+    <div className='head'>
+      <h2>Login</h2>
+      <div className="input-Email">
+        <label>Email :</label>
+        <input type="email" value={userEmail} onChange={(e) => setEmail(e.target.value)} />
       </div>
 
-      <div className='dk'>
-        <label className='lj'>Password :  </label>
+      <div className="input-Password">
+        <label>Password :</label>
         <input type="password" value={userPassword} onChange={(e) => setPassword(e.target.value)} />
       </div>
       
-      <div className='ls'>{error && <p style={{ color: 'red' }}>{error}</p>}</div>
-      <div className='dk'><button onClick={handleSignIn} disabled={loading}>
-        {loading ? 'Logging In...' : 'Login'}
-      </button></div>
-     <div className='dk'>
-      <a href='http://localhost:5173/Sign'>
-      <button className='li'>Sign Up </button>
+      <div>{error && <p style={{ color: 'red' }}>{error}</p>}</div>
+      
+      <div>
+        <button onClick={handleSignIn} disabled={loading}>
+          {loading ? 'Logging In...' : 'Login'}
+        </button>
+      </div>
 
-      </a>
-     </div>
- 
+      <div>
+        <button onClick={handleSignUp}>Sign Up</button>
+      </div>
     </div>
   );
 };
