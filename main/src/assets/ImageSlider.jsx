0159307bbff86ca07603251c6670afc998958caf
Abhinav352 <abhinav0298@gmail.com>
2024-04-11 21:55:39 +0530

    Update ImageSlider.jsx

--- main/src/assets/ImageSlider.jsx
+++ main/src/assets/ImageSlider.jsx
@@ -1,6 +1,7 @@
 import _default from "@mui/material/styles/identifier";
 import SimpleImageSlider from "react-simple-image-slider";
 import './Image.css'
+import { Link } from "react-router-dom";
 const images = [
   { url: "src/images/earthq.jpg"},
   { url: "src/images/hurri.png" },
@@ -10,12 +11,15 @@ const images = [
 
 
 const ImageSlider = () => {
+
+  
   return (
+    <Link to="/response" >
     <div className="main">
         <div className="inner">
       <SimpleImageSlider
-        width={496} 
-        height={400}
+        width={700} 
+        height={450}
         images={images}
         showBullets={true}
         showNavs={true}
@@ -23,14 +27,15 @@ const ImageSlider = () => {
       </div>
       <div className="inner2">
       <SimpleImageSlider
-        width={296} 
-        height={200}
+        width={400} 
+        height={400}
         images={images}
         showBullets={true}
         showNavs={true}
       />
       </div>
     </div>
+    </Link>
   );
 }
 export default ImageSlider;
